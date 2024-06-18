db7048b38aafcd3059035346ad7e8dc0f62d2b4e
Ross Cohen <rosscohen1990@gmail.com>
2024-06-17 22:03:03 -0400

    url

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
   description:
     "Crafting Chatbots with OpenAI's Assistant.",
   url: "https://open-assistant-gpt.vercel.app/",
-  ogImage: "https://www.openassistantgpt.io/dashboard.png",
+  ogImage: "https://img.mytsi.org/i/PcNe636.png",
   links: {
     twitter: "https://twitter.com/tsi_org",
     github: "https://github.com/Tech-in-Schools-Initiative",
